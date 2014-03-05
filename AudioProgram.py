9ff07039a4f61876842bfa69c30c69780966d2a9
Nathan Kistler <nkistler@csumb.edu>
2014-03-05 15:18:40 -0800

    Added new libraries, default playlist values, and audio playback capability

--- AudioProgram.py
+++ AudioProgram.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python
 import glob, re, os
+import pyglet
 from mutagen.mp3 import MP3
 from mutagen.id3 import ID3, TCON
 
-def play():
+def generate():
+    for entry in allFileNamesSet:
+        play(entry)
+    return
+    
+def play(fileName):
+    filePath = "Audio/" + fileName
+    music = pyglet.resource.media(filePath)
+    music.play()
+    pyglet.app.run()
     return
 
 def select():
-
-    #variables that persist within scope of function
-    allFileNamesList = []
-    totalNumberOfFiles = 0
-    homeDir = "/home/nathan/workspace/AudioProgram/Audio/"
-    homeDirList = os.listdir(homeDir)
-
     #take input
     artist = raw_input("Enter Artist name: ")
     genre = raw_input("Enter Genre name: ")
@@ -24,26 +27,33 @@ def select():
             genreTag = ID3(homeDir + entry).getall('TCON')
             artistTag = ID3(homeDir + entry).getall('TPE1') + ID3(homeDir + entry).getall('TPE1')
             if genre in [genreTag[0]]:
-                allFileNamesList.insert(totalNumberOfFiles, entry)
-	        totalNumberOfFiles=totalNumberOfFiles+1
+                allFileNamesSet.add(entry) 
             if artist in [artistTag[0]]:
-                allFileNamesList.insert(totalNumberOfFiles, entry)
-	        totalNumberOfFiles=totalNumberOfFiles+1
+                allFileNamesSet.add(entry)
             if artist in [artistTag[1]]:
-                allFileNamesList.insert(totalNumberOfFiles, entry)
-	        totalNumberOfFiles=totalNumberOfFiles+1
-            print allFileNamesList
+                allFileNamesSet.add(entry)
+    print allFileNamesSet
     return
-    
+
+#global variables
+allFileNamesSet= set()
+homeDir = "/home/nathan/workspace/AudioProgram/Audio/"
+homeDirList = os.listdir(homeDir)
+selection = ""
+
+#initialize globals
+for entry in homeDirList:
+    if re.search(r'.mp3', entry):
+        allFileNamesSet.add(entry)
+print allFileNamesSet
 
 #main program loop
-selection = ""
 while selection.strip() != 'quit':
-    print "Press 1 to play"
-    print "Press 2 to select"
+    print "Press 1 to generate audio"
+    print "Press 2 to select audio criteria"
     selection = raw_input("Please enter command: ")
     print "You entered ", selection
     if selection=="1":
-        play() #executes rotate function
+        generate() #plays files which match current criteria
     elif selection=="2":
-        select() #executes create thumbnail function
+        select() #allows user to modify current audio generation criteria
