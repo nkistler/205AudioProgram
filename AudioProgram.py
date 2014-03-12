fcda5bcd647679f4df8da54661eae9fae771d8f7
Nathan Kistler <nkistler@csumb.edu>
2014-03-12 15:21:23 -0700

    Can now search recursively within homeDir.

--- AudioProgram.py
+++ AudioProgram.py
@@ -1,29 +1,41 @@
 #!/usr/bin/python
+#needed libraries
 import re, os, sys, random, pygame
 from pygame.locals import *
 from mutagen.mp3 import MP3
 from mutagen.id3 import ID3
 
 #global variables
-allFileNamesSet= set()
+selectedFilePathsSet= set()
 userArtistList = []
 userGenreList = []
-homeDir = "/home/nathan/workspace/AudioProgram/Audio/"
-homeDirList = os.listdir(homeDir)
+homeDir = "/home/nathan/workspace/AudioProgram/Audio" #DO NOT include trailing slash
+homeDirList = list()
 selection = ""
 
 #initialize globals
-for entry in homeDirList:
-    if re.search(r'.mp3', entry):
-        allFileNamesSet.add(entry) #This gives us a non-empty playlist to start with before we even enter our preferences.
+#this starts our audio modules
 pygame.init()
 pygame.mixer.init()
+
+#This allows us to recursively search our "home" directory. Places all possible audio file paths in homeDirList.
+def getAllHomeFolder(x, dir_name, files): 
+    for item in files:
+        if re.search(r'.mp3', item):
+            homeDirList.append(dir_name + "/" + item)
+os.path.walk(homeDir, getAllHomeFolder, 0)
+
+#This gives us a non-empty playlist to start with before we even enter our preferences
+for entry in homeDirList:
+    if re.search(r'.mp3', entry):
+        selectedFilePathsSet.add(entry) 
+
 
 #main functions
 def play():
      #randomize the sample of music
     randomizedSample = set()
-    randomizedSample = random.sample(allFileNamesSet, len(allFileNamesSet))
+    randomizedSample = random.sample(selectedFilePathsSet, len(selectedFilePathsSet))
 
     #print playlist
     print "Playlist:"
@@ -39,7 +51,7 @@ def play():
 
     #start playback and ask for next input
     while len(randomizedSample) > 0:
-        pygame.mixer.music.load(homeDir + randomizedSample.pop())
+        pygame.mixer.music.load(randomizedSample.pop())
         pygame.mixer.music.play(0)
         while pygame.mixer.music.get_busy():
             print "Enter command \"stop\" to stop player, \"next\" to skip to next track, or \"quit\" to exit."
@@ -53,7 +65,10 @@ def play():
                     print "Error: End of playlist"
                 else:
                     nameOfCurrentFile = randomizedSample[len(randomizedSample)-1]
-                    print "Current track: " + nameOfCurrentFile
+                    artistTag = getArtistTag(nameOfCurrentFile)
+                    titleTag = getTitleTag(nameOfCurrentFile)
+                    genreTag = getGenreTag(nameOfCurrentFile)
+                    print "Current track: " +  artistTag + ", " + titleTag + ", " + genreTag
                     break
             elif selection.strip().lower()=='quit':
                 sys.exit()
@@ -78,7 +93,7 @@ def select():
     print userGenreList
 
     #empty the current set
-    allFileNamesSet.clear()
+    selectedFilePathsSet.clear()
 
     #perform search
     for entry in homeDirList:
@@ -90,26 +105,25 @@ def select():
 
             #check tags against user created lists
             for item in userArtistList:
-                if (re.search(item, artistTag.lower())): # Need to fix this, possibly has something to do with paces?
-                    allFileNamesSet.add(entry)
+                if (re.search(item, artistTag.lower())):
+                    selectedFilePathsSet.add(entry)
             for item in userGenreList:
                 if re.search(item, genreTag.lower()):
-                    allFileNamesSet.add(entry)
+                    selectedFilePathsSet.add(entry)
     return
 
 #getters and setters
-def getArtistTag(fileName):
-    artistTag = ID3(homeDir + fileName).getall('TPE1')
+def getArtistTag(pathName):
+    artistTag = ID3(pathName).getall('TPE1')
     return str(artistTag[0])
 
-def getGenreTag(fileName):
-    genreTag = ID3(homeDir + fileName).getall('TCON')
+def getGenreTag(pathName):
+    genreTag = ID3(pathName).getall('TCON')
     return str(genreTag[0])
 
-def getTitleTag(fileName):
-    titleTag = ID3(homeDir + fileName).getall('TIT2')
+def getTitleTag(pathName):
+    titleTag = ID3(pathName).getall('TIT2')
     return str(titleTag[0])
-    
 
 #main program loop
 while selection.strip().lower() != 'quit':
